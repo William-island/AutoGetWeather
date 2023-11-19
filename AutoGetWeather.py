387d617988a245e0bd2b943878e88fa700b4bd89
William-island <18761099420@163.com>
2023-11-19 21:35:50 +0800

    debug autoGetWeather

--- AutoGetWeather.py
+++ AutoGetWeather.py
@@ -10,21 +10,32 @@ from bs4 import BeautifulSoup
 # define the function to get weather of nanjing, china
 def get_weather():
     # city code
-    rul = 'http://www.weather.com.cn/weather/101190104.shtml'
-    r = requests.get(rul)
+    url = 'https://tianqi.2345.com/jiangning2d/70447.htm'
+    r = requests.get(url)
     r.encoding = 'utf-8'
     bs = BeautifulSoup(r.text, 'html.parser')
-    data_tommorrow = bs.find_all('li', class_='sky skyid lv3')[0]
-    date, weather, temperature, wind = data_tommorrow.text.split()
-    context = f'Date: {date}\nWeather: {weather}\nTemperature: {temperature}\nWind: {wind}'
-    return context
+    data_tommorrow = bs.find_all('div', class_='real-mess')[0]
+    # Extracting the weather information
+    date_info = data_tommorrow.find('p', {'class': 'real-wea-info'}).text
+    temperature = data_tommorrow.find('span', {'class': 'real-t'}).text
+    air_quality = data_tommorrow.find('span', {'class': 'real-rank'}).text
+    date, tomorrow_weather = data_tommorrow.find('div', {'class': 'real-today'}).text.split("：")
+    wind_info = data_tommorrow.find('em', {'class': 'wea-info-wind'}).parent.text
+    humidity_info = data_tommorrow.find('em', {'class': 'wea-info-humidity'}).parent.text
+    uv_info = data_tommorrow.find('em', {'class': 'wea-info-sunblack'}).parent.text
+    pressure_info = data_tommorrow.find('em', {'class': 'wea-info-pa'}).parent.text
+
+    # organize context
+    content = f"Date Info: {date} {date_info}\nTomorrow's Weather: {tomorrow_weather}\nAVG Temperature: {temperature}\nAir Quality: {air_quality}\n\Wind Info: {wind_info}\nHumidity Info: {humidity_info}\nUV Info: {uv_info}\nPressure Info: {pressure_info}\n"
+    #print(context)
+    return content
 
 # auto email
 def send_email(content):
     mail_host = "smtp.qq.com"  # 填写邮箱服务器:这个是qq邮箱服务器，直接使用smtp.qq.com
     mail_pass = 'chlzwavkcodtbfge'  # 填写在qq邮箱设置中获取的授权码
     sender = '943649026@qq.com'  # 填写邮箱地址
-    receivers = ['18761099420@163.com']  # 填写收件人的邮箱，QQ邮箱或者其他邮箱，可多个，中间用,隔开
+    receivers = ['18761099420@163.com','1270142056@qq.com']  # 填写收件人的邮箱，QQ邮箱或者其他邮箱，可多个，中间用,隔开
 
     subject = 'AutoWeather'  #发送的主题
         
@@ -32,7 +43,7 @@ def send_email(content):
     message['Subject'] = Header(subject, 'utf-8')
 
     message['From'] = Header("William_from_github <943649026@qq.com>")  # 'utf-8'  #邮件发送者姓名 
-    message['To'] = Header("William")    #邮件接收者姓名
+    message['To'] = Header("Weather Info")    #邮件接收者姓名
 
 
     smtpObj = smtplib.SMTP_SSL(mail_host, 465) #建立smtp连接，qq邮箱必须用ssl边接，因此边接465端口
